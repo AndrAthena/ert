00f382395c879a663ec9b25670d0c7e7d4b52dc0
AndrAthena <andrianantenaina.nandrianina@gmail.com>
2021-08-17 10:57:37 +0300

    Init project

---
+++ src/Theme/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@material-ui/core';
+
+export default createTheme({
+  typography: {
+    fontFamily: "'Poppins', sans-serif",
+    fontSize: 16,
+    htmlFontSize: 16,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+  },
+});
