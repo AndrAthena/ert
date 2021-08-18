112b52d61d28c2007c905eca3e37636c7d6e9148
AndrAthena <andrianantenaina.nandrianina@gmail.com>
2021-08-18 17:39:47 +0300

    new password

--- src/components/Authentication/NewPassword/NewPassword.tsx
+++ src/components/Authentication/NewPassword/NewPassword.tsx
@@ -1,7 +1,41 @@
 import { FC } from 'react';
+import SEO from '../../Common/SEO';
+import FormContainer from '../FormContainer';
+import Illustration from '../../../assets/images/password-reset.svg';
+import PasswordInput from '../../Common/PasswordInput';
+import { Button, Divider } from '@material-ui/core';
+import CustomLink from '../../Common/CustomLink';
 
 const NewPassword: FC = () => {
-  return <div></div>;
+  return (
+    <>
+      <SEO title="New Password" />
+      <FormContainer title="Reset" description="Enter your new password" className="m-auto" illustration={Illustration} alt="Login illustration">
+        <form>
+          <PasswordInput label="New password" />
+          <PasswordInput label="Confirm new password" />
+
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+            Reset password
+          </Button>
+
+          <Divider variant="fullWidth" className="mt-5 mb-4" />
+
+          <CustomLink
+            href="/login"
+            button
+            buttonProps={{
+              variant: 'outlined',
+              fullWidth: true,
+              color: 'primary',
+            }}
+          >
+            Login
+          </CustomLink>
+        </form>
+      </FormContainer>
+    </>
+  );
 };
 
 export default NewPassword;
