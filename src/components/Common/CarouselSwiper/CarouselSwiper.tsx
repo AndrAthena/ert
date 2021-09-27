abfd32c41b84be6323103217c8d0ab577548b100
AndrAthena <andrianantenaina.nandrianina@gmail.com>
2021-09-27 21:20:07 +0300

    update caroussel swiper

--- src/components/Common/CarouselSwiper/CarouselSwiper.tsx
+++ src/components/Common/CarouselSwiper/CarouselSwiper.tsx
@@ -2,14 +2,22 @@ import React, { FC, ReactNode } from 'react';
 import 'swiper/swiper.min.css';
 import { SwiperOptions } from 'swiper/types';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper as Swipe, Navigation, Mousewheel } from 'swiper';
 
 interface CarouselSwiperProps extends SwiperOptions {
   slides: ReactNode[];
 }
 
+Swipe.use([Navigation, Mousewheel]);
+
 const CarouselSwiper: FC<CarouselSwiperProps> = ({ slides, ...rest }) => {
   return (
-    <Swiper {...rest}>
+    <Swiper
+      mousewheel={{
+        sensitivity: 0.5,
+      }}
+      {...rest}
+    >
       {slides.map((slide: any, index: number) => {
         return <SwiperSlide key={`swiper-slide-${index}`}>{slide}</SwiperSlide>;
       })}
