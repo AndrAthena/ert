9846a921c61097c9f4bbeaad437ffef91d8bdf3c
AndrAthena <andrianantenaina.nandrianina@gmail.com>
2021-10-05 22:04:18 +0300

    stepper add mouse scroll

--- src/components/Common/CustomStepper/CustomStepper.tsx
+++ src/components/Common/CustomStepper/CustomStepper.tsx
@@ -5,16 +5,24 @@ import CustomLink from '../CustomLink';
 import styles from './style';
 
 interface CustomStepperprops {
-  active: number;
-  stepNumber: number;
+  title: string;
+  subtitle: string;
+  active?: number;
+  stepNumber?: number;
 }
 
-const CustomStepper: FC<CustomStepperprops> = ({ active, stepNumber }) => {
+const CustomStepper: FC<CustomStepperprops> = ({ active, stepNumber, title, subtitle }) => {
   const cls = styles();
-  const progress: number = (active / stepNumber) * 100;
+  const progress: number = stepNumber && active ? (active / stepNumber) * 100 : 0;
 
   return (
-    <AppBar color="inherit" elevation={0} position="sticky" component="div" classes={{ root: 'mb-5' }}>
+    <AppBar
+      color="inherit"
+      elevation={0}
+      position="sticky"
+      component="div"
+      classes={{ root: 'mb-5' }}
+    >
       <Toolbar classes={{ root: 'container' }} disableGutters>
         <div className="d-flex align-items-center">
           <CustomLink style={{ color: 'inherit' }} href="/create-template">
@@ -22,20 +30,28 @@ const CustomStepper: FC<CustomStepperprops> = ({ active, stepNumber }) => {
           </CustomLink>
           <div className="ml-2">
             <Typography color="textPrimary">
-              <strong>New template</strong>
+              <strong>{title}</strong>
             </Typography>
             <Typography variant="body2" color="textSecondary">
-              Create a new template quickly
+              {subtitle}
             </Typography>
           </div>
         </div>
         <div className={cls.stepText}>
           <Typography color="textPrimary" align="center">
-            Step {active}/{stepNumber}
+            {stepNumber && active && (
+              <>
+                Step {active}/{stepNumber}
+              </>
+            )}
           </Typography>
         </div>
       </Toolbar>
-      <LinearProgress variant="determinate" value={progress} style={{ width: '100%', position: 'absolute', bottom: 0 }} />
+      <LinearProgress
+        variant="determinate"
+        value={progress}
+        style={{ width: '100%', position: 'absolute', bottom: 0 }}
+      />
     </AppBar>
   );
 };
